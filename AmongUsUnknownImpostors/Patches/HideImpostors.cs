73efb70b93dac52ada5c1b44ce2ecbc418b04bc9
Antho_Off <113532396+AnthoYt@users.noreply.github.com>
2025-02-20 15:41:22 +0100

    Update HideImpostors.cs

--- AmongUsUnknownImpostors/Patches/HideImpostors.cs
+++ AmongUsUnknownImpostors/Patches/HideImpostors.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Text;
+using System;
 using HarmonyLib;
-using UnhollowerBaseLib;
 using UnityEngine;
+using UnhollowerBaseLib;
 
 namespace AmongUsUnknownImpostors.Patches
 {
     internal class HideImpostors
     {
+        // Patch to hide impostors' names in chat
         [HarmonyPatch(typeof(ChatController), nameof(ChatController.AddChat))]
         public static class ChatController_AddChat
         {
@@ -17,7 +15,7 @@ namespace AmongUsUnknownImpostors.Patches
             {
                 PlayerControl sourcePlayer = KMCAKLLFNIM;
 
-                if (!sourcePlayer || !PlayerControl.LocalPlayer)
+                if (sourcePlayer == null || PlayerControl.LocalPlayer == null)
                 {
                     return;
                 }
@@ -31,76 +29,69 @@ namespace AmongUsUnknownImpostors.Patches
 
                 var activeChildren = __instance.chatBubPool.activeChildren;
 
+                // Cast the last chat bubble to the appropriate type
                 ChatBubble chatBubble = activeChildren[activeChildren.Count - 1].Cast<ChatBubble>();
 
+                // Hide impostors' names in chat if the player is an impostor
                 if (data2.IsImpostor && data2.Object != PlayerControl.LocalPlayer)
-                    chatBubble.NameText.Color = UnityEngine.Color.white;
+                    chatBubble.NameText.Color = Color.white;
             }
         }
 
-        //Patch that makes impostors along in intro cutscene
+        // Patch to make impostors alone in the intro cutscene
         [HarmonyPatch(typeof(IntroCutscene), nameof(IntroCutscene.BeginImpostor))]
         public static class IntroCutscene_BeginImpostor
         {
-            public static void Prefix(IntroCutscene __instance,
-                ref Il2CppSystem.Collections.Generic.List<PlayerControl> KADFCNPGKLO)
+            public static void Prefix(IntroCutscene __instance, ref Il2CppSystem.Collections.Generic.List<PlayerControl> KADFCNPGKLO)
             {
                 if (!CustomGameOptionsData.customGameOptions.unkImpostor.value) return;
+
                 var yourTeam = KADFCNPGKLO;
                 yourTeam.Clear();
-                yourTeam.Add(PlayerControl.LocalPlayer);
+                yourTeam.Add(PlayerControl.LocalPlayer);  // Only the local player is considered an impostor
             }
         }
 
-        //Patch that hide other impostors in Meeting HUD
+        // Patch to hide other impostors in the meeting HUD
         [HarmonyPatch(typeof(MeetingHud), nameof(MeetingHud.Method_7))]
         public static class MeetingHud_CreateButton
         {
-            public static void Postfix(MeetingHud __instance, GameData.PlayerInfo PPIKPNJEAKJ,
-                ref PlayerVoteArea __result)
+            public static void Postfix(MeetingHud __instance, GameData.PlayerInfo PPIKPNJEAKJ, ref PlayerVoteArea __result)
             {
                 if (!CustomGameOptionsData.customGameOptions.unkImpostor.value) return;
+
                 GameData.PlayerInfo playerInfo = PPIKPNJEAKJ;
                 if (playerInfo.IsImpostor && playerInfo.Object != PlayerControl.LocalPlayer)
-                    __result.NameText.Color = UnityEngine.Color.white;
+                    __result.NameText.Color = Color.white;  // Hide impostors' names in the meeting HUD
             }
         }
 
-        //Patch that fixes Kill button between impostors
+        // Patch to fix the kill button between impostors
         [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.FindClosestTarget))]
         public static class PlayerControl_FindClosestTarget
         {
-            private static int ShipAndObjectsMask = LayerMask.GetMask(new string[]
-            {
-                "Ship",
-                "Objects"
-            });
+            private static int ShipAndObjectsMask = LayerMask.GetMask(new string[] { "Ship", "Objects" });
 
             public static bool Prefix(PlayerControl __instance, ref PlayerControl __result)
             {
                 if (!CustomGameOptionsData.customGameOptions.unkImpostor.value) return true;
+
                 PlayerControl result = null;
-                float num = GameOptionsData.KillDistances[
-                    Mathf.Clamp(PlayerControl.GameOptions.KillDistance, 0, 2)];
-                if (!ShipStatus.Instance)
-                {
-                    return true;
-                }
+                float num = GameOptionsData.KillDistances[Mathf.Clamp(PlayerControl.GameOptions.KillDistance, 0, 2)];
+                if (!ShipStatus.Instance) return true;
 
                 Vector2 truePosition = __instance.GetTruePosition();
                 for (int i = 0; i < GameData.Instance.AllPlayers.Count; i++)
                 {
                     GameData.PlayerInfo playerInfo = GameData.Instance.AllPlayers[i];
-                    if (!playerInfo.Disconnected && playerInfo.PlayerId != __instance.PlayerId &&
-                        !playerInfo.IsDead)
+                    if (!playerInfo.Disconnected && playerInfo.PlayerId != __instance.PlayerId && !playerInfo.IsDead)
                     {
                         PlayerControl @object = playerInfo.Object;
                         if (@object)
                         {
                             Vector2 vector = @object.GetTruePosition() - truePosition;
                             float magnitude = vector.magnitude;
-                            if (magnitude <= num && !PhysicsHelpers.AnyNonTriggersBetween(truePosition,
-                                vector.normalized, magnitude, ShipAndObjectsMask))
+                            if (magnitude <= num && !PhysicsHelpers.AnyNonTriggersBetween(truePosition, vector.normalized, magnitude, ShipAndObjectsMask))
                             {
                                 result = @object;
                                 num = magnitude;
@@ -110,24 +101,25 @@ namespace AmongUsUnknownImpostors.Patches
                 }
 
                 __result = result;
-                return false;
+                return false;  // Prevent the original method from executing
             }
         }
 
-        //Patch that sets player name color when impostors are chosen
+        // Patch to set player name color when impostors are chosen
         [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.SetInfected))]
         public static class PlayerControl_RpcSetInfected
         {
             public static void Postfix(PlayerControl __instance, Il2CppStructArray<byte> JPGEIBIBJPJ)
             {
                 if (!CustomGameOptionsData.customGameOptions.unkImpostor.value) return;
+
                 var infected = JPGEIBIBJPJ;
                 for (int j = 0; j < infected.Length; j++)
                 {
                     GameData.PlayerInfo playerById2 = GameData.Instance.GetPlayerById(infected[j]);
                     if (playerById2 != null && playerById2.Object != PlayerControl.LocalPlayer)
                     {
-                        playerById2.Object.nameText.Color = UnityEngine.Color.white;
+                        playerById2.Object.nameText.Color = Color.white;  // Hide impostors' names in the game
                     }
                 }
             }
