91d32d2970881c941389b2ccb91ecd734779f2e6
Antho_Off <113532396+AnthoYt@users.noreply.github.com>
2025-02-20 15:39:11 +0100

    Update LightSabotageFix.cs

--- AmongUsUnknownImpostors/Patches/LightSabotageFix.cs
+++ AmongUsUnknownImpostors/Patches/LightSabotageFix.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System;
 using HarmonyLib;
 using UnityEngine;
 
@@ -11,28 +9,42 @@ namespace AmongUsUnknownImpostors.Patches
         [HarmonyPatch(typeof(ShipStatus), nameof(ShipStatus.CalculateLightRadius))]
         public static class ShipStatus_CalculateLightRadius
         {
-            public static bool Prefix(ShipStatus __instance, GameData.PlayerInfo IIEKJBMPELC, ref float __result)
+            public static bool Prefix(ShipStatus __instance, GameData.PlayerInfo playerInfo, ref float __result)
             {
+                // Si l'option "Unk impostor" est désactivée, on laisse la méthode originale s'exécuter
                 if (!CustomGameOptionsData.customGameOptions.unkImpostor.value) return true;
-                var player = IIEKJBMPELC;
-                if (player == null || player.IsDead)
+
+                // On vérifie que le joueur existe et s'il est mort
+                if (playerInfo == null || playerInfo.IsDead)
                 {
                     __result = __instance.MaxLightRadius;
-                    return false;
+                    return false; // On empêche l'exécution de la méthode originale
                 }
 
-                SwitchSystem switchSystem = __instance.Systems[SystemTypes.Electrical].Cast<SwitchSystem>();
-                float num = (float) switchSystem.Value / 255f;
-                if (player.IsImpostor)
+                // Récupération du système électrique
+                if (__instance.Systems[SystemTypes.Electrical] is SwitchSystem switchSystem)
                 {
-                    __result = Mathf.Lerp(__instance.MinLightRadius, __instance.MaxLightRadius, num) *
-                               Mathf.Lerp(CustomGameOptionsData.customGameOptions.impoVision.value, PlayerControl.GameOptions.ImpostorLightMod, num);
-                    return false;
+                    // Calcul du facteur d'intensité des lumières basé sur l'état du système électrique
+                    float lightFactor = (float)switchSystem.Value / 255f;
+
+                    if (playerInfo.IsImpostor)
+                    {
+                        // Calcul de la lumière pour les imposteurs
+                        __result = Mathf.Lerp(__instance.MinLightRadius, __instance.MaxLightRadius, lightFactor) *
+                                   Mathf.Lerp(CustomGameOptionsData.customGameOptions.impoVision.value, PlayerControl.GameOptions.ImpostorLightMod, lightFactor);
+                    }
+                    else
+                    {
+                        // Calcul de la lumière pour les membres de l'équipage
+                        __result = Mathf.Lerp(__instance.MinLightRadius, __instance.MaxLightRadius, lightFactor) *
+                                   PlayerControl.GameOptions.CrewLightMod;
+                    }
+
+                    return false; // On empêche l'exécution de la méthode originale
                 }
 
-                __result = Mathf.Lerp(__instance.MinLightRadius, __instance.MaxLightRadius, num) *
-                           PlayerControl.GameOptions.CrewLightMod;
-                return false;
+                // Si le système électrique n'est pas valide, on laisse la méthode originale s'exécuter
+                return true;
             }
         }
     }
